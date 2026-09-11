82072acd5f9716a1c2c5fc7c3036678208e6d465
Sean Paul Campbell <seanpc@iastate.edu>
2026-09-11 10:53:18 -0500

    bug fix

--- boolforge/boolean_network/dynamics_async.py
+++ boolforge/boolean_network/dynamics_async.py
@@ -283,6 +283,7 @@ class BooleanNetworkDynamicsAsyncMixin:
         else:
             _build_sdds_transition_csr_method = _build_sdds_transition_csr
 
+        STG_synchronous_exact, _ = self._get_property("STG", context="synchronous")
         indptr, indices, data = _build_sdds_transition_csr_method(
             STG_synchronous_exact,
             p_degradation=p_degradation,
